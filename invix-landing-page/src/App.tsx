9fe16c0a4b6268b9b6dad0ca049d34171dd4c5a6
george ibrahim hanif <122005621+JorLojor@users.noreply.github.com>
2025-09-28 17:51:07 +0700

    chore(init): hadeh

---
+++ invix-landing-page/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+	return (
+		<div className="flex h-screen items-center justify-center bg-blue-600">
+			<h1 className="text-5xl font-bold text-white">Tailwind v4 🚀</h1>
+		</div>
+	);
+}
